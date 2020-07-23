c3233036737c300bcbaa09920c962e16cfdb5648
Mateus Araujo <mateusdearaujo@hotmail.com.br>
2020-07-22 21:50:34 -0300

    Criando página principal

---
+++ index.php
@@ -0,0 +1,42 @@
+<?php
+session_start();
+include "config.php";
+
+if(empty($_SESSION['lg'])) {
+    header("Location: login.php");
+    exit;
+} else {
+    $id = $_SESSION['lg'];
+    $ip = $_SERVER['REMOTE_ADDR'];
+
+    $sql = $pdo->prepare("SELECT * FROM usuarios WHERE id = :id AND ip = :ip");
+    $sql->bindValue(":id", $id);
+    $sql->bindValue(":ip", $ip);
+    $sql->execute();
+
+    if($sql->rowCount() == 0) {
+        header("Location: login.php");
+        exit;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Login</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<main>
+<div class="container">
+    <h1>Conteúdo Confidencial</h1>
+</div>
+</main>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+</body>
+</html>
